23ebefb34e36c59db08b7f13a7f34ae28c95af6a
João Pereira <joaopereira9913@gmail.com>
2026-02-27 21:49:31 +0000

    Added variables and exercises

---
+++ 01_basics/01_variables.py
@@ -0,0 +1,38 @@
+"""
+01_variables.py
+Topic: Variables & basic data types
+Goal: Learn how to declare variables and access their values
+"""
+
+# ------------------
+# Numeric Types
+# ------------------
+int_number = 2
+float_numer = 3.1
+complex_number = 10+3j
+
+# ------------------
+# Sequence Types
+# ------------------
+string_example = "Hello, World"
+list_example = [1, 2, 3, 4, 5]
+tuple_example = (1, 2, 3, 4, 5)
+
+# ------------------
+# Set
+# ------------------
+set_example = {1, 2, 3, 4, "Hello, World"}
+
+# ------------------
+# Dictionary
+# ------------------
+dict_example = {
+    "name": "Joao",
+    "client": "Company",
+    "age": 26
+}
+
+# ------------------
+# Boolean
+# ------------------
+boolean_example = True
